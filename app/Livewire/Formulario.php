f98fd90071ae247d2a8734c7462d2f3efee8def3
Abraham Romero <romero9804@hotmail.com>
2025-07-31 22:18:57 -0700

    feat: crear formulario de post

--- app/Livewire/Formulario.php
+++ app/Livewire/Formulario.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use App\Models\Tag;
 use Livewire\Component;
 
 class Formulario extends Component
 {
+    public $categories, $tags;
+    public $category_id, $title, $content;
+    public $selectedTags = [];
+
+    public function mount()
+    {
+        $this->categories = Category::all();
+        $this->tags = Tag::all();
+    }
+
+    public function save()
+    {
+        dd([
+            'category_id' => $this->category_id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'selectedTags' => $this->selectedTags,
+        ]);
+    }
     public function render()
     {
         return view('livewire.formulario');
--- resources/views/livewire/formulario.blade.php
+++ resources/views/livewire/formulario.blade.php
@@ -1,3 +1,44 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="bg-white shadow rounded-lg p-6">
+        <h2 class="font-semibold text-lg text-gray-800 mb-6">Formulario</h2>
+        <form wire:submit="save" novalidate>
+            <div>
+                <x-label for="title" value="Titulo" />
+                <x-input id="title" type="text" class="mt-1 block w-full" autofocus wire:model="title" />
+            </div>
+            <div>
+                <x-label for="content" value="Contenido" />
+                <x-textarea id="content" class="mt-1 block w-full" rows="3" wire:model="content">
+                    {{ old('content') }}
+                </x-textarea>
+            </div>
+            <div>
+                <x-label for="category_id" value="Categoria" />
+                <x-select id="category_id" class="mt-1 block w-full" wire:model="category_id">
+                    <option value="" disabled>Select Category</option>
+                    @foreach($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </x-select>
+            </div>
+            <div>
+                <x-label for="tags" value="Tags" />
+                <ul>
+                    @foreach($tags as $tag)
+                        <li>
+                            <label for="{{$tag->id}}" value="{{$tag->name}}">
+                                <x-checkbox id="{{$tag->id}}" value="{{$tag->id}}" wire:model="selectedTags" />
+                                {{$tag->name}}
+                            </label>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="mt-6 flex justify-end">
+                <x-button type="submit">
+                    Submit
+                </x-button>
+            </div>
+        </form>
+    </div>
 </div>
