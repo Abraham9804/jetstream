a9cb58607979cecb39f8ae4faff365f8473244f4
Abraham Romero <romero9804@hotmail.com>
2025-08-07 20:27:16 -0700

    feat: add validation and error handling for post creation form

--- resources/views/livewire/formulario.blade.php
+++ resources/views/livewire/formulario.blade.php
@@ -2,17 +2,19 @@
     <div class="bg-white shadow rounded-lg p-6 mb-8">
         <h2 class="font-semibold text-lg text-gray-800 mb-6">Formulario</h2>
         <form wire:submit="save" novalidate>
-            <div>
+            <div class="mb-4">
                 <x-label for="title" value="Titulo" />
                 <x-input id="title" type="text" class="mt-1 block w-full" autofocus wire:model="title" />
+                <x-input-error for="title"/>
             </div>
-            <div>
+            <div class="mb-4">
                 <x-label for="content" value="Contenido" />
                 <x-textarea id="content" class="mt-1 block w-full" rows="3" wire:model="content">
                     {{ old('content') }}
                 </x-textarea>
+                <x-input-error for="content" />
             </div>
-            <div>
+            <div class="mb-4">
                 <x-label for="category_id" value="Categoria" />
                 <x-select id="category_id" class="mt-1 block w-full" wire:model="category_id">
                     <option value="" disabled>Select Category</option>
@@ -20,8 +22,9 @@
                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                     @endforeach
                 </x-select>
+                <x-input-error for="category_id" />
             </div>
-            <div>
+            <div class="mb-4">
                 <x-label for="tags" value="Tags" />
                 <ul>
                     @foreach($tags as $tag)
@@ -30,6 +33,7 @@
                                 <x-checkbox id="{{$tag->id}}" value="{{$tag->id}}" wire:model="selectedTags" />
                                 {{$tag->name}}
                             </label>
+                            <x-input-error for="selectedTags.{{$tag->id}}" />
                         </li>
                     @endforeach
                 </ul>
@@ -76,18 +80,18 @@
         <x-slot name="content">
             <form wire:submit="update" novalidate>
                 <div>
-                    <x-label for="title" value="Titulo" />
-                    <x-input id="title" type="text" class="mt-1 block w-full" autofocus wire:model="postEdit.title" />
+                    <x-label for="titleEdit" value="Titulo" />
+                    <x-input id="titleEdit" type="text" class="mt-1 block w-full" autofocus wire:model="postEdit.title" />
                 </div>
                 <div>
-                    <x-label for="content" value="Contenido" />
-                    <x-textarea id="content" class="mt-1 block w-full" rows="3" wire:model="postEdit.content">
+                    <x-label for="contentEdit" value="Contenido" />
+                    <x-textarea id="contentEdit" class="mt-1 block w-full" rows="3" wire:model="postEdit.content">
                         {{ old('content') }}
                     </x-textarea>
                 </div>
                 <div>
-                    <x-label for="category_id" value="Categoria" />
-                    <x-select id="category_id" class="mt-1 block w-full" wire:model="postEdit.category_id">
+                    <x-label for="category_idEdit" value="Categoria" />
+                    <x-select id="category_idEdit" class="mt-1 block w-full" wire:model="postEdit.category_id">
                         <option value="" disabled>Select Category</option>
                         @foreach($categories as $category)
                             <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -99,8 +103,8 @@
                     <ul>
                         @foreach($tags as $tag)
                             <li>
-                                <label for="{{$tag->id}}" value="{{$tag->name}}">
-                                    <x-checkbox id="{{$tag->id}}" value="{{$tag->id}}" wire:model="postEdit.tags"/>
+                                <label for="{{$tag->id}}Edit" value="{{$tag->name}}">
+                                    <x-checkbox id="{{$tag->id}}Edit" value="{{$tag->id}}" wire:model="postEdit.tags"/>
                                     {{$tag->name}}
                                 </label>
                             </li>
